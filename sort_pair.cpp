e3076cb926a096004055c9483c30ef2f0cda494d
Niharika <niharika14.11.2004@gmail.com>
2024-08-08 17:25:31 +0530

    print sorted pair in decreasing order

--- sort_pair.cpp
+++ sort_pair.cpp
@@ -5,14 +5,14 @@ int main(){
     vector<pair<int, string>> v; // {scores, name}
     
     v.push_back({90, "niharika"});
-    v.push_back({92, "nisha"});
+    v.push_back({90, "nisha"});
     v.push_back({85, "reena"});
     v.push_back({87, "manvi"});
     v.push_back({85, "aashi"});
 
     sort(v.begin(), v.end());
 
-    for (int i = 0, n = v.size(); i < n; i++)
+    for (int n = v.size(), i = n - 1; i >= 0; i--)
     {
         cout << v[i].second << endl;
     }
