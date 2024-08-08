134992e01945885bbc8684e5e9221bbf3e9f3644
Niharika <niharika14.11.2004@gmail.com>
2024-08-08 16:34:14 +0530

    sort an array using sort fn

---
+++ sort.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[] = {5,4,1,2};
+    sort(a, a+4);
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << a[i] << "\n";
+    }
+}
