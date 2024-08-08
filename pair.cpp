3b23d2a6d8a1a5db9e40ae985aacf1141e63f13d
Niharika <niharika14.11.2004@gmail.com>
2024-08-08 17:03:41 +0530

    learn using pair datatype

---
+++ pair.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Return a pair with a function
+// pair<int, int> fn(){
+//     return {3, 4};
+// }
+
+int main()
+{
+    pair<string, int> p = {"niharika", 14};
+
+    cout << p.first << " " << p.second <<endl;
+}
