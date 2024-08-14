e54b2e1c2223b497e54e2040d0cd8ae12707566f
Niharika <niharika14.11.2004@gmail.com>
2024-08-14 21:22:03 +0530

    solved kattis first problem

---
+++ ug/general/basket.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int l = s.size();
+    char lastletter = s[l - 2];
+    cout << lastletter << "\n";
+}
