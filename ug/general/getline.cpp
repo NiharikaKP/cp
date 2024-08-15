29c0ddc1ef50b453e782d672a78a8b2e8d58e023
Niharika <niharika14.11.2004@gmail.com>
2024-08-15 18:29:54 +0530

    getline function

---
+++ ug/general/getline.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string line;
+    cout << "Type some text, and press enter: " << "\n";
+    getline(cin, line);
+
+    cout << "You Typed " << line << "\n";
+}
